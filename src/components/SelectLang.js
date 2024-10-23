952b9e4c0810a7f84f9ad738943abb049717954e
Martins Onifade <negrosmartins@gmail.com>
2024-10-23 17:08:41 +0100

    Added few more language option to test it in app

--- src/components/SelectLang.js
+++ src/components/SelectLang.js
@@ -7,6 +7,8 @@ function SelectLang({ Lang, setLang }) {
                 <option value={"en"}>English</option>
                 <option value="fr">French</option>
                 <option value="es">Spanish</option>
+                <option value="de">German</option>
+                <option value="pt">Portuguese</option>
             </select>
         </div>
     );
