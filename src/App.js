3d2e3bb6817e2fa92f1108fde64fabc626f0c7c7
Martins Onifade <negrosmartins@gmail.com>
2024-10-23 16:32:35 +0100

    improved app.js by including usestate to implement language selection

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import TextInput from './components/TextInput';
 import React from 'react';
 import { useState } from 'react';
+import SelectLang from './components/SelectLang';
+
 
 function App() {
   const [Input, setInput] = useState('');
   const [TranslatedInput, setTranslatedInput] = useState('');
+  const [FromLang, setFromLang] = useState("en");
+  const [ToLang, setToLang] = useState("fr");
 
 
   const GetTranslation = async () => {
-    const translations = await TranslatedInput(Input)
+    const translations = await TranslatedInput(Input, FromLang, ToLang);
     setTranslatedInput(translations)
   };
   return (
     <div className="App">
       <h1>Welcome to YouTranslate</h1>
       <TextInput input={Input} setinput={setInput}/>
+      <SelectLang/>
     </div>
   );
 }
---
+++ src/components/SelectLang.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SelectLang({ Lang, setLang }) {
+    return (
+        <div>
+            <select></select>
+        </div>
+    )
+}
+
+export default SelectLang
