bf402cb6c9a910770baa42328067c4e472910dfd
Martins Onifade <negrosmartins@gmail.com>
2024-10-23 16:03:24 +0100

    created the translation state abd handled the translation using async and await

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { useState } from 'react';
 
 function App() {
   const [Input, setInput] = useState('');
- //const [TranslatedInput, setTranslatedInput] = useState('');
+  const [TranslatedInput, setTranslatedInput] = useState('');
 
 
-  
+  const GetTranslation = async () => {
+    const translations = await TranslatedInput(Input)
+    setTranslatedInput(translations)
+  };
   return (
     <div className="App">
       <h1>Welcome to YouTranslate</h1>
