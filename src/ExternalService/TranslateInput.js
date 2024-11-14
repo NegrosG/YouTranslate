878fb527fd893accb0325a60e70224b07bf42fe2
Martins Onifade <negrosmartins@gmail.com>
2024-11-14 11:49:23 +0100

    few new changes

--- src/ExternalService/TranslateInput.js
+++ src/ExternalService/TranslateInput.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const TranslateInput = async (Input, FromLang, ToLang) => {
     try {
-        const result = await axios.post('../api/translate', {
+        const result = await axios.post('/api/translate', {
             text: Input,
             sourceLanguage: FromLang,
             targetLanguage: ToLang
         });
 
-        if (result.data.translations && result.data.translations.lenght > 0) {
+        if (result.data.translations && result.data.translations.length > 0) {
             return result.data.translations[0].text;
         } else {
             console.error("Unexpected response format:", result.data);
